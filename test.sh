5a8e79cbfbdf61f79972b3e6a202bac31b012bad
David Elentok <3david@gmail.com>
2023-07-24 20:48:07 +0300

    test: shallow clone plenary
    Makes the first test run much faster.
    
    Also, add .testcache/ to .gitignore

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ plugins_dir="./.testcache/site/pack/vendor/start"
 
 if ! [ -e "${plugins_dir}/plenary.nvim" ]; then
   mkdir -p .testcache/site
-  git clone https://github.com/nvim-lua/plenary.nvim "${plugins_dir}/plenary.nvim"
+  git clone --depth=1 https://github.com/nvim-lua/plenary.nvim "${plugins_dir}/plenary.nvim"
 fi
 
 nvim --headless --noplugin -u tests/init.lua -c "lua require('plenary.test_harness').test_directory('tests/format-on-save/', {minimal_init='tests/init.lua',sequential=true})"
